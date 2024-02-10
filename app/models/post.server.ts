5c83a0495d4762c9220fe06ab44bb21492b6ea47
Alan Uecker <uecker.alan@gmail.com>
2024-02-10 12:25:42 +0100

    added some markdown plugins

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,7 +1,11 @@
 import type { Post } from '@prisma/client';
 
-import { bundleMDX } from '../utils/mdx.server';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePresetMinify from 'rehype-preset-minify';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 import { prisma } from '../utils/db.server';
+import { bundleMDX } from '../utils/mdx.server';
 
 export async function getPost({ id }: Pick<Post, 'id'>) {
   const post = await prisma.post.findFirst({
@@ -9,7 +13,20 @@ export async function getPost({ id }: Pick<Post, 'id'>) {
     where: { id },
   });
 
-  const { code } = await bundleMDX({ source: post?.content || '' });
+  const { code } = await bundleMDX({
+    source: post?.content || '',
+    mdxOptions(options, frontmatter) {
+      options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
+      options.rehypePlugins = [
+        ...(options.rehypePlugins ?? []),
+        rehypeSlug,
+        rehypeAutolinkHeadings,
+        rehypePresetMinify,
+      ];
+
+      return options;
+    },
+  });
 
   return { ...post, code };
 }

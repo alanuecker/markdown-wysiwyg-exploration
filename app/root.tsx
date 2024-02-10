473de2c0a40ef725e9e8f1ab07aceb1aaf3d773f
Alan Uecker <uecker.alan@gmail.com>
2024-02-10 12:25:58 +0100

    added global css styling

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import styles from './shared.css';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  ...(cssBundleHref
+    ? [
+        { rel: 'stylesheet', href: cssBundleHref },
+        { rel: 'stylesheet', href: styles },
+      ]
+    : []),
 ];
 
 export default function App() {
